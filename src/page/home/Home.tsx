3e6c66410055585c479b196013ea4f3307bde4e3
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-10-06 12:22:00 +0200

    new home design

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
-import {AppBar, Box, Button, Container, Grid, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Box,
+    Card,
+    CardActionArea, CardContent,
+    CardMedia,
+    Container,
+    Grid,
+    Toolbar,
+    Typography
+} from "@mui/material";
 
 
 export function Home() {
@@ -29,15 +39,39 @@ export function Home() {
                     </Box>
                 </Grid>
                 <Container className="container">
-                    <Grid item xs={8}>
-                        <Button variant="contained" onClick={goToMealsDashboard}>
-                            Meals dashboard
-                        </Button>
+                    <Grid item xs={8} className="container">
+                        <Card sx={{ maxWidth: 360 }} onClick={goToMealsDashboard} className={"horizontally-center"}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="240"
+                                    image="https://dhqbz5vfue3y3.cloudfront.net/fotomondobb/2006_top.jpg"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography variant="h5" component="div">
+                                        Meals dashboard
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
                     </Grid>
-                    <Grid item xs={8}>
-                        <Button variant="contained" onClick={goToItemDashboard}>
-                            Item dashboard
-                        </Button>
+                    <Grid item xs={8} className="container">
+                        <Card sx={{ maxWidth: 360 }} onClick={goToItemDashboard} className={"horizontally-center"}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="240"
+                                    image="https://hips.hearstapps.com/hmg-prod/images/healthy-groceries-1525213305.jpg"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography variant="h5" component="div">
+                                        Grocery dashboard
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
                     </Grid>
                 </Container>
             </Grid>
