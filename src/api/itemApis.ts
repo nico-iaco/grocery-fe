2dabff51df3a2f859b3511d7f919e194267f2480
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-12-26 18:16:17 +0100

    added firebase token on grocery-be api calls 🔒

--- src/api/itemApis.ts
+++ src/api/itemApis.ts
@@ -5,15 +5,20 @@ import {Transaction} from "../model/transaction";
 import {FoodDetail} from "../model/foodDetails";
 import {ItemStatistics} from "../model/itemStatistics";
 import {ShoppingItem} from "../model/shoppingItem";
+import {getFirebaseUserToken} from "../utils/firebaseUtils";
 
 export const baseUrl = process.env.REACT_APP_BASE_URL
 
 export const addItem = async (item: Item, controller: AbortController) => {
     const url = `${baseUrl}/item/`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.post(
         url,
         item,
         {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            },
             signal: controller.signal,
         }
     );
@@ -23,6 +28,7 @@ export const addItem = async (item: Item, controller: AbortController) => {
 
 export const addShoppingItemList = async (shoppingItemList: ShoppingItem[], controller: AbortController) => {
     const url = `${baseUrl}/item/all`;
+    const token = await getFirebaseUserToken()
     const request = {
         shoppingItems: shoppingItemList
     }
@@ -30,6 +36,9 @@ export const addShoppingItemList = async (shoppingItemList: ShoppingItem[], cont
         url,
         request,
         {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            },
             signal: controller.signal,
         }
     );
@@ -39,9 +48,10 @@ export const addShoppingItemList = async (shoppingItemList: ShoppingItem[], cont
 
 export const getAllItems = async (onlyAvailable: boolean = false, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/?onlyAvailable=${onlyAvailable}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.get(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -54,9 +64,10 @@ export const getAllItems = async (onlyAvailable: boolean = false, userid: string
 
 export const getItemStatistics = async (userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/statistics`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.get(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -69,9 +80,10 @@ export const getItemStatistics = async (userid: string, controller: AbortControl
 
 export const getItemDetail = async (itemId: string, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${itemId}/detail`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.get(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -84,9 +96,10 @@ export const getItemDetail = async (itemId: string, userid: string, controller: 
 
 export const updateItem = async (item: Item, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${item.id}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.patch(url, item, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -99,9 +112,10 @@ export const updateItem = async (item: Item, userid: string, controller: AbortCo
 
 export const deleteItem = async (id: string, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${id}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.delete(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -114,9 +128,10 @@ export const deleteItem = async (id: string, userid: string, controller: AbortCo
 
 export const getFoodKcal = async (foodId: string, quantity: number, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${foodId}/kcal?quantity=${quantity}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.get(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -129,9 +144,10 @@ export const getFoodKcal = async (foodId: string, quantity: number, userid: stri
 
 export const addTransactionToItem = async (itemId: string, transaction: Transaction, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${itemId}/transaction`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.post(url, transaction, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -144,9 +160,10 @@ export const addTransactionToItem = async (itemId: string, transaction: Transact
 
 export const getAllItemTransaction = async (itemId: string, onlyAvailable: boolean = false, userId: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${itemId}/transaction?onlyAvailable=${onlyAvailable}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.get(url, {
         headers: {
-            "iv-user": userId
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -159,9 +176,10 @@ export const getAllItemTransaction = async (itemId: string, onlyAvailable: boole
 
 export const updateItemTransaction = async (itemId: string, transaction: Transaction, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${itemId}/transaction`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.patch(url, transaction, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
@@ -174,9 +192,10 @@ export const updateItemTransaction = async (itemId: string, transaction: Transac
 
 export const deleteItemTransaction = async (itemId: string, transactionId: string, userid: string, controller: AbortController) => {
     const url = `${baseUrl}/item/${itemId}/transaction/${transactionId}`;
+    const token = await getFirebaseUserToken()
     const axiosResponse = await axios.delete(url, {
         headers: {
-            "iv-user": userid
+            "Authorization": `Bearer ${token}`
         },
         signal: controller.signal,
     });
