6c4db1eff4ca684e98c99f63d51a943e67f23d75
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2023-02-05 11:37:09 +0100

    Migrating from CRA to Vite build 🚀

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,10 +3,10 @@ interface ImportMetaEnv {
     readonly VITE_BASE_URL: string;
     readonly VITE_VERSION: string;
     readonly VITE_AVAILABLE_UNITS: string;
-    readonly REACT_APP_API_KEY: string;
-    readonly REACT_APP_MESSAGING_SENDER_ID: string;
-    readonly REACT_APP_APP_ID: string;
-    readonly REACT_APP_MEASUREMENT_ID: string;
+    readonly VITE_API_KEY: string;
+    readonly VITE_MESSAGING_SENDER_ID: string;
+    readonly VITE_APP_ID: string;
+    readonly VITE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
